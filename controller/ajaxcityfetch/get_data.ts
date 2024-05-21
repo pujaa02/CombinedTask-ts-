13a7de09ce9a9ad4fa20b7b5b94f9c5b6f13f92b
puja.morvadiya <puja.moravadiya2024@gmail.com>
2024-05-21 12:35:31 +0530

    changed js to ts

--- controller/ajaxcityfetch/get_data.ts
+++ controller/ajaxcityfetch/get_data.ts
@@ -3,7 +3,7 @@ import { Request, Response } from "express";
 import { RowDataPacket } from "mysql2";
 
 function get_state(req: Request, res: Response) {
-  con.query(`select * from states`, async function (err:Error, result:Array<RowDataPacket>, fields) {
+  con.query(`select * from states`, async function (err:Error, result:Array<RowDataPacket>) {
     if (err) throw err;
     const data:Array<RowDataPacket>= await result;
     res.json(data);
